a35d5d4288b5d437fb12035834071ae119ffefde
王恆 <wangheng@wanghengdeMacBook-Air-2.local>
2025-09-04 22:39:20 +0800

    Add new functionalites and correct error of OECD def.
    New functionalities include rendering MS² spectra in a mirror format and
    providing a simple demonstration of DeePFAS through the addition of a
    graphical user interface (GUI). Additionally, the errors in OECD.py have been corrected.

--- DeePFAS/DeePFAS/utils/OECD.py
+++ DeePFAS/DeePFAS/utils/OECD.py
@@ -1,5 +1,6 @@
 from rdkit import Chem
 
+
 def oecd_pfas(smiles):
 
     # follow revised OCED definition
@@ -13,8 +14,15 @@ def oecd_pfas(smiles):
     for atom in mol.GetAtoms():
         # Check for perfluorinated methyl group (-CF3)
         if atom.GetSymbol() == 'C' and atom.GetDegree() >= 2:
-            neighbors = [nbr.GetSymbol() for nbr in atom.GetNeighbors()]
-            if neighbors.count('F') >= 2 and not any([nbr in ['H', 'Br', 'I', 'Cl'] for nbr in neighbors]):
+            neighbors = [nbr for nbr in atom.GetNeighbors()]
+            valid_f = 0
+            for nbr in neighbors:
+                if nbr.GetSymbol() == 'F' and len(nbr.GetNeighbors()) == 1:
+                    valid_f += 1
+            if valid_f in [2, 3] and not \
+                any([bond.GetBondType() != Chem.BondType.SINGLE for bond in atom.GetBonds()]) and not \
+                any([nbr.GetSymbol() in ['H', 'Br', 'I', 'Cl'] for nbr in neighbors]):
+                        
                 return True  # Perfluorinated methyl group found
 
     return False
